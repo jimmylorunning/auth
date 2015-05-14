96582c37e4828470c365cd33c6914ee5c54e4fdc
jimmylorunning <jimmylorunning@gmail.com>
2015-05-13 20:52:54 -0400

    Starting over with auth.test.php

--- auth.test.php
+++ auth.test.php
@@ -1,8 +1,23 @@
 <?php
+require_once 'auth.class.php';
+require_once 'config.inc.php';
+
+// http://code.tutsplus.com/tutorials/evolving-toward-a-persistence-layer--net-27138
+
 class AuthTest extends PHPUnit_Framework_TestCase
 {
-  public function testCreateUser() {
+  private $auth;
+  private $pdo;
 
+  public function setUp() {
+    global $db, $dbconfig;
+    $this->pdo = Auth::newPdo($db, $dbconfig);
+    $this->auth = new Auth($pdo);
+  }
+
+  public function testCreateUser() {
+    $this->cleanUpDatabase();
+/*
     $mockdb = $this->getMockBuilder('myDb')
       ->setMethods(array('insert'))
       ->getMock();
@@ -12,9 +27,16 @@ class AuthTest extends PHPUnit_Framework_TestCase
       ->with($this->equalTo('users'));
 
     // create user
-    $auth = new Auth($mockdb);
-    $auth->createUser("jimmy", "password");
-    
+    $this->auth->createUser("jimmy", "password");
+ */   
+  }
+
+  private function cleanUpDatabase() {
+    $this->pdo->exec("DELETE FROM `users`"); 
+    $this->pdo->exec("DELETE FROM `user_sessions`");
+    $this->pdo->exec("INSERT INTO `users` (email,password,user_salt,is_active,is_admin) VALUES ('jimmylospelunking@gmail.com','0c8c69bf9b40caf931c77925ad274a86fed6382ab210849ef26c80cdc87d75da1d5654a4bfa8836f55596b072c523f033de2113cdc09184805134bb2d1c7233f','A{iaD%EtjTt4Ppd74YSCB6Bs4m!euRWB6yKNAFkJ52jG$<xJa*',1,0)");
+    // keep this one login for testing purposes:
+    //   jimmylospelunking@gmail / spelunking 
   }
 }
 ?>
