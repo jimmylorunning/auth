25e345286193ff5c620285c97db7ab532945556c
jimmylorunning <jimmylorunning@gmail.com>
2015-09-03 19:01:37 -0400

    Remove database stuff from auth unit tests, tests should not touch database

--- tests/auth.test.php
+++ tests/auth.test.php
@@ -1,10 +1,7 @@
 <?php
 
-require_once 'shared/database.inc.php';
 require_once 'classes/auth.class.php';
-require_once 'classes/connectionfactory.class.php';
 
-// http://code.tutsplus.com/tutorials/evolving-toward-a-persistence-layer--net-27138
 /**
  * @backupGlobals disabled
  * @backupStaticAttributes disabled
@@ -15,22 +12,19 @@ class AuthTest extends PHPUnit_Framework_TestCase
   private $usr;
   private $pdo;
 
-  // The setUp() and tearDown() template methods are run once for each test method (and on fresh instances) of the test case class.
   public function setUp() {
-    $this->pdo = ConnectionFactory::getFactory()->getConnection();
     $this->usr = $this->getMockBuilder('User')
                 ->setMethods(array('import', 'create'))
                 ->getMock();
     $this->usr_gw = $this->getMockBuilder('UserGateway')
                 ->setMethods(array('existsBy'))
                 ->getMock();
-    $this->usr_session = $this->getMockBuilder('Wendell')
+    $this->usr_session = $this->getMockBuilder('Sesh')
                 ->getMock();                
     $this->auth = new Auth($this->usr, $this->usr_session, $this->usr_gw);
   }    
 
   public function testCreateUserCallsUserCreate() {
-    $this->cleanUpDatabase();
     $this->usr_gw->method('existsBy')->willReturn(false);
     $this->usr->expects($this->once())
       ->method('create');
@@ -38,7 +32,6 @@ class AuthTest extends PHPUnit_Framework_TestCase
   }
 
  public function testCreateUserReturnsSuccess() {
-    $this->cleanUpDatabase();
     $this->usr_gw->method('existsBy')->willReturn(false);
     $this->usr->method('create')
       ->willReturn(true);
@@ -47,7 +40,6 @@ class AuthTest extends PHPUnit_Framework_TestCase
   }
 
   public function testDuplicateUserShouldFail() {
-    $this->cleanUpDatabase();
     $this->usr_gw->method('existsBy')->willReturn(true);
     $this->usr->method('create')
       ->willReturn(true);
@@ -55,22 +47,4 @@ class AuthTest extends PHPUnit_Framework_TestCase
     $this->assertEquals(Auth::USER_EXISTS, $authcode);
   }
 
-  public function tearDownOld() {
-    $this->cleanUpDatabase();
-    $this->pdo->exec("INSERT INTO `users` (email,password,user_salt,is_active,is_admin) VALUES ('jimmylospelunking@gmail.com','0c8c69bf9b40caf931c77925ad274a86fed6382ab210849ef26c80cdc87d75da1d5654a4bfa8836f55596b072c523f033de2113cdc09184805134bb2d1c7233f','A{iaD%EtjTt4Ppd74YSCB6Bs4m!euRWB6yKNAFkJ52jG$<xJa*',1,0)");
-    // keep this one login for testing purposes:
-    //   jimmylospelunking@gmail / spelunking 
-  }
-
-  private function cleanUpDatabase() {
-    $this->pdo->exec("DELETE FROM `users`"); 
-    $this->pdo->exec("DELETE FROM `user_sessions`");
-  }
-
-  private function getUsersFromDatabase() {
-    $result = $this->pdo->query("SELECT * FROM `users`");
-    return $result->fetch(PDO::FETCH_ASSOC);
-  }
 }
-/* @runTestsInSeparateProcesses
-*/
